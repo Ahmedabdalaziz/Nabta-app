63e414d02c3c12503d3e7b89673032eb0f3f50b5
Ahmedabdalaziz <ahmedabdalziz.1886@gmail.com>
2024-11-12 11:13:50 +0200

    23oso mraba

---
+++ lib/features/signup/data/model/custom/sigin_model_error.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sigin_model_error.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SignUpErrorModel _$SignUpErrorModelFromJson(Map<String, dynamic> json) =>
+    SignUpErrorModel(
+      json['message'] as String,
+    );
+
+Map<String, dynamic> _$SignUpErrorModelToJson(SignUpErrorModel instance) =>
+    <String, dynamic>{
+      'message': instance.message,
+    };
